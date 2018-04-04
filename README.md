cd4d552cd4166ac55bf65c33f11bac6a3d682ea1
OnlineAlex <becasualua@gmail.com>
2018-04-04 12:28:31 +0300

    рабочая версия скритпта / написал README

--- README.md
+++ README.md
@@ -1,7 +1,45 @@
-# Coursera Dump
+# Данные о курсах на Курсере
 
-[TODO. There will be project description]
+Скрипт собирает данные о курсах на [coursera.org](https://www.coursera.org/) и сохраняет их в .xlsm фаил в указанную папку.
 
-# Project Goals
+Колличество курсов: 20
 
-The code is written for educational purposes. Training course for web-developers - [DEVMAN.org](https://devman.org)
+Тип данных: название, язык (включая субтитры), дату начала, длительность (в неделях), рейтинг, ссылку.
+
+# Как работает 
+```bash
+$ python coursera.py A:\myfiles
+Файл сохранен
+A:\myfiles\courses.xlsx
+```
+
+# Требования
+Совестимые OC:
+* Linux,
+* Windows
+* MacOS
+
+Скрипт требует для своей работы установленного интерпретатора Python версии 3.5 выше
+
+И  пакетов из requirements.txt
+```bash
+pip install -r requirements.txt # или командой pip3
+```
+# Как запустить
+Стандатной командой `python` (на некоторых компьютерах `python3`)
+
+```bash
+$ python coursera.py [-h] filedir
+ 
+positional arguments:
+  filedir     Путь для сохранения файла
+ 
+optional arguments:
+  -h, --help  show this help message and exit
+```
+> Запуск для всех ОС одинаковый
+
+Помните, рекомендуется использовать [virtualenv/venv](https://devman.org/encyclopedia/pip/pip_virtualenv/) для лучшего управления пакетами.
+
+# Цели проекта
+Код создан в учебных целях. В рамках учебного курса по веб-разработке - [DEVMAN.org](https://devman.org)
--- coursera.py
+++ coursera.py
@@ -1,16 +1,113 @@
+import requests
+from bs4 import BeautifulSoup
+from openpyxl import Workbook
+import os
+import argparse
 
 
-def get_courses_list():
-    pass
+def is_valid_path(argument_path):
+    if not os.path.exists(argument_path):
+        error_message = 'Папка {} не существует'.format(argument_path)
+        raise argparse.ArgumentTypeError(error_message)
+
+    return argument_path
 
 
-def get_course_info(course_slug):
-    pass
+def get_filedir():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        'filedir',
+        type=is_valid_path,
+        help='Путь для сохранения файла'
+    )
+    return parser.parse_args()
 
 
-def output_courses_info_to_xlsx(filepath):
-    pass
+def get_courses_list(number_courses):
+    response = requests.get(
+        'https://www.coursera.org/sitemap~www~courses.xml',
+    )
+    courses_xml = BeautifulSoup(response.content, features='xml')
+    courses = courses_xml.text.split()
+    return courses[:number_courses]
+
+
+def get_course_info(course_url):
+    response = requests.get(course_url)
+    response.encoding = 'utf-8'
+    course_code = BeautifulSoup(response.text, 'html.parser')
+    startdate_text = course_code.select_one('.startdate').text
+    rating = course_code.select_one('.ratings-text')
+    if rating:
+        rating = rating.text[:3]
+
+    return {
+        'title': course_code.select_one('.title').text,
+        'language': course_code.select_one('.rc-Language').text,
+        'date start': startdate_text[startdate_text.find(' ')+1:],
+        'number week': len(course_code.select('.week')),
+        'rating': rating,
+        'url': course_url
+    }
+
+
+def get_colums_max_width(table):
+    colums_width = {}
+    for row in table.rows:
+        for cell in row:
+                colums_width[cell.column] = max((
+                    colums_width.get(cell.column, 0),
+                    len(str(cell.value))
+                ))
+    return colums_width
+
+
+def output_courses_info_to_xlsx(courses, filepath):
+    courses_workbook = Workbook()
+    courses_sheet = courses_workbook.active
+    courses_sheet.append([
+        'Название',
+        'Язык',
+        'Дата начала',
+        'Длительность (нед)',
+        'Средняя оценка',
+        'Ссылка'
+    ])
+
+    for course in courses:
+        courses_sheet.append([
+            course['title'],
+            course['language'],
+            course['date start'],
+            course['number week'],
+            course['rating'],
+            course['url']
+        ])
+
+    colums_max_width = get_colums_max_width(courses_sheet)
+    for column, value in colums_max_width.items():
+        courses_sheet.column_dimensions[column].width = value
+
+    courses_workbook.save(filepath)
+    if os.path.exists(filepath):
+        return True
 
 
 if __name__ == '__main__':
-    pass
+    arguments = get_filedir()
+    xlsx_filepath = os.path.join(arguments.filedir, 'courses.xlsx')
+
+    try:
+        courses_list = get_courses_list(number_courses=20)
+        courses_info = []
+        for course in courses_list:
+            courses_info.append(get_course_info(course))
+    except requests.ConnectionError:
+        exit('Не удалось подключиться к серверу\n'
+             'Проверьте интернет соеденние')
+
+    if output_courses_info_to_xlsx(courses_info, xlsx_filepath):
+        print('Файл сохранен\n{}'.format(xlsx_filepath))
+    else:
+        print('Что-то пошло не так. Файл не сохранен.')
+
